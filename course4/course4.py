a34d55066cd374fc0d1abe09be73cc22ea3f30e0
Michael Dufour <mikerhinosfr@gmail.com>
2016-05-13 15:54:48 +0200

    Fin Course4 Signed-off-by:Michael Dufour <mikerhinosfr@gmail.com>

---
+++ course4/course4.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup
+import urllib.request
+import time
+
+req = urllib.request.urlopen("http://www.lemonde.fr/politique/rss_full.xml")
+xml = BeautifulSoup(req, "xml")
+
+for item in xml.findAll("link")[3:]:
+    url = item.text
+    news = urllib.request.urlopen(url).read()
+    page = BeautifulSoup(news, "lxml")
+    
+    for p in page.findAll("p"):
+        print(p.text)
+        time.sleep(5)
